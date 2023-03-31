006ee8fc0a32c0bb042ad735b8c1c4f0d689b1c9
Mohammedodejimi@gmail.com <Mohammedodejimi@gmail.com>
2023-03-31 16:19:39 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const openMenu = document.querySelector('.openmenu')
+const closeMenu = document.querySelector('.closemenu')
+
+
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', close)
+
+function show () {
+  mainMenu.style.display = 'flex'
+  mainMenu.style.top = '0'
+}
+function close () {
+  mainMenu.style.top = '100%'
+}
+
+
